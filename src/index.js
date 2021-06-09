f6c10628a36aec8cba5399aee7f619e4ab31d95f
xiaosasori <xiaosasori@gmail.com>
2021-06-09 17:58:18 +0900

    conditional require dotenv

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // eslint-disable-next-line
-require('dotenv').config();
+process.env.NODE_ENV === 'development' && require('dotenv').config();
 const express_1 = __importDefault(require("express"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const apollo_server_express_1 = require("apollo-server-express");
